459512b350e933ec777c0e34fb4ee42b71b99290
NanaQuaci <aducollins49@gmail.com>
2023-06-25 23:29:24 +0000

    bugs fixed on June 25th

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../../components/Input';
 import { Form, Link, useSubmit } from 'react-router-dom';
+import { Formik, useFormik } from 'formik';
 import Button from '../../components/Button';
 import logo from '../../assets/png/Aser.png';
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate, useHistory } from 'react-router-dom';
 
 const Login = () => {
-  
-  const navigate = useNavigate() 
-  const handleSubmit = () =>{
-    navigate("/")
-    
-  }
+
+  const formik =useFormik({
+    initialValues: {
+      fname: "",
+      lname: "",
+      email: "",
+      password: "",
+    },
+  })
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+
+    const username = document.getElementById('username')?.value;
+    const password = document.getElementById('password')?.value;
+    //const history = useHistory();
+    // Check that username and password fields are not empty
+    if (!username || !password) {
+      alert('Please enter both a username and password.');
+      return;
+    }
+
+    // Check that password meets complexity requirements
+    if (
+      password.length < 8 ||
+      !/\d/.test(password) ||
+      !/[a-zA-Z]/.test(password)
+    ) {
+      alert(
+        'Password must be at least 8 characters long and contain both letters and numbers.'
+      );
+
+      console.log('Login succes');
+      navigate('/');
+    }
+  };
   return (
     <div className="main-container">
       <div className="d-flex">
@@ -20,40 +50,42 @@ const Login = () => {
           <img src={logo} alt="" className="img-fluid" width="200" />
         </div>
         <div className="right ">
-        <form onSubmit={handleSubmit}>
+        <Formik>
 
-          <div className="card border-0 login_card">
-            <h5>LOG INTO YOUR ACCOUNT</h5>
-            <h6>Welcome back</h6>
-            <Input
-              type="email"
-              placeholder="Email Address"
-              className=" mt-4"
-            />
-            <Input type="password" placeholder="Password" className=" mt-4" />
-            <div className="d-flex justify-content-between">
-              <div></div>
-              <a href="" className="text-decoration-none text-secondary my-2">
-                Forgot Password
-              </a>
+          <form onSubmit={handleSubmit}>
+            <div className="card border-0 login_card">
+              <h5>LOG INTO YOUR ACCOUNT</h5>
+              <h6>Welcome back</h6>
+              <Input
+                type="email"
+                placeholder="Email Address"
+                className=" mt-4"
+                onChange={() => formik.handleChange('email')}
+              />
+              <Input type="password" placeholder="Password" className=" mt-4" />
+              <div className="d-flex justify-content-between">
+                <div></div>
+                <a href="" className="text-decoration-none text-secondary my-2">
+                  Forgot Password
+                </a>
+              </div>
+              <Button btnName="Sign in" type="submit" />
+              <p className="my-3">
+                Don't have an account?{' '}
+                <Link
+                  to="/signup"
+                  className="text-decoration-none my-2"
+                  style={{ color: 'red' }}
+                >
+                  Sign up Now
+                </Link>
+              </p>
             </div>
-            <Button btnName="Sign in" type="submit" />
-            <p className="my-3">
-              Don't have an account?{' '}
-              <Link
-                to="/signup"
-                className="text-decoration-none my-2"
-                style={{ color: 'red' }}
-              >
-                Sign up Now
-              </Link>
-            </p>
-          </div>
-        </form>
+          </form>
+        </Formik>
         </div>
       </div>
     </div>
   );
 };
-
 export default Login;
--- src/pages/Auth/SignUp.js
+++ src/pages/Auth/SignUp.js
@@ -3,8 +3,23 @@ import Input from '../../components/Input';
 import { Link } from 'react-router-dom';
 import Button from '../../components/Button';
 import logo from '../../assets/png/Aser.png';
+import { useState } from 'react';
+import axios from 'axios';
 
 const SignUp = () => {
+
+    const [firstname, setfName] = useState();
+    const [lastname, setlName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+
+    const handleSubmit = (e) =>{
+      e.preventDefault();
+      axios.post('http://localhost:3000/signup', {firstname, lastname, email, password})
+      .then(result => console.log(result))
+      .catch(err => console.log(err))
+    }
   return (
     <div className="main-container">
       <div className="d-flex">
@@ -13,19 +28,26 @@ const SignUp = () => {
             <h5>CREATE AN ACCOUNT</h5>
             <h6>Join now to enjoy exclusiveness</h6>
             <div className="d-flex signup_input">
+            <form onSubmit={handleSubmit} />
               <Input
                 type="text"
                 placeholder="First Name"
                 className=" mt-4 inp"
+                onChange={(e) => setfName(e.tartget.value)}
               />
               <Input
                 type="text"
                 placeholder="Last Name"
                 className=" mt-4 inp"
+                onChange={(e) => setlName(e.tartget.value)}
               />
             </div>
-            <Input type="email" placeholder="Email Address" className=" mt-4" />
-            <Input type="password" placeholder="Password" className=" mt-4" />
+            <Input type="email" placeholder="Email Address" className=" mt-4" 
+              onChange={(e) => setEmail(e.tartget.value)}
+            />
+            <Input type="password" placeholder="Password" className=" mt-4" 
+              onChange={(e) => setPassword(e.tartget.value)}
+            />
             <div className="d-flex justify-content-between">
               <div className="my-3">
                 <Input type="checkbox" /> I have read the{' '}
